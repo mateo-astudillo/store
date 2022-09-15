76053f27f0bbb62b5f4bbe146ff22c1a9115203c
Mateo Astudillo <ast.mateo@gmail.com>
2022-09-15 01:15:30 -0300

    Makefile

--- ac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm ex
-files="src/main.c src/menu.c src/session/menu-logged.c src/crud/create.c \
-  src/crud/read.c src/crud/update.c src/session/login.c src/user/see-balance.c src/user/modify-user.c src/init/init.c"
-gcc -o ex $files -lsqlite3 && ./ex
